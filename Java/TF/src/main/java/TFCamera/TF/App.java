c4ba46c79f94065360bb4a1aaea3c080dfca5c5c
bokoto000 <bobi_velk@abv.bg>
2018-03-07 23:24:44 +0200

    Add files via upload

---
+++ Java/TF/src/main/java/TFCamera/TF/App.java
@@ -0,0 +1,13 @@
+package TFCamera.TF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
