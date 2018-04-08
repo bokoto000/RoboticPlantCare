f4f8839f916f66ede894318e2587756e5158d5f3
bokoto000 <bobi_velk@abv.bg>
2018-04-08 23:46:32 +0300

    Add files via upload

---
+++ Raspberry/Car.py
@@ -0,0 +1,85 @@
+import RPi.GPIO as GPIO
+import time, datetime
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+#Droid
+interval = 0.1
+PWMA = 5
+AIN1 = 13
+AIN2 = 6
+PWMB = 23
+BIN1 = 24
+BIN2 = 25
+STBY = 19
+
+rotor = { 'PWMA', 'AIN1', 'AIN2', 'PWMB', 'BIN1', 'BIN2', 'STBY' }
+
+
+GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setup(PWMA, GPIO.OUT)
+GPIO.setup(BIN1, GPIO.OUT)
+GPIO.setup(BIN2, GPIO.OUT)
+GPIO.setup(PWMB, GPIO.OUT)
+GPIO.setup(STBY, GPIO.OUT)
+
+
+
+class Car(object):
+
+    def right(self,duration):
+        (GPIO.output(AIN1, GPIO.HIGH))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(PWMA, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.HIGH))
+        (GPIO.output(BIN2, GPIO.LOW))
+        (GPIO.output(PWMB, GPIO.HIGH))
+        (GPIO.output(STBY, GPIO.HIGH))
+        time.sleep(duration)
+        stop()
+        
+
+    def left(self,duration):
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.HIGH))
+        (GPIO.output(PWMA, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.HIGH))
+        (GPIO.output(PWMB, GPIO.HIGH))
+        (GPIO.output(STBY, GPIO.HIGH))
+        time.sleep(duration)
+        stop()
+
+    def back(self,duration):
+        (GPIO.output(AIN1, GPIO.HIGH))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(PWMA, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.HIGH))
+        (GPIO.output(PWMB, GPIO.HIGH))
+        (GPIO.output(STBY, GPIO.HIGH))
+        time.sleep(duration)
+        stop()
+
+
+    def forward(self,duration):
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.HIGH))
+        (GPIO.output(PWMA, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.HIGH))
+        (GPIO.output(BIN2, GPIO.LOW))
+        (GPIO.output(PWMB, GPIO.HIGH))
+        (GPIO.output(STBY, GPIO.HIGH))
+        time.sleep(duration)
+        stop()
+
+    def stop(self):
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(PWMA, GPIO.LOW))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.LOW))
+        (GPIO.output(PWMB, GPIO.LOW))
+        (GPIO.output(STBY, GPIO.LOW))
